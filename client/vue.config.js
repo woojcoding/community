0e047cace4b48a3df78f6282794f3c03ad89cb78
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-09-01 14:15:26 +0900

    vuejs 빌드 위치 지정
    빌드 위치를 community-user의 static폴더로 지정하여 index파일을 해당 폴더에 생성하도록 지정하였음.

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, '../server-user/src/main/resources/static')
 })
