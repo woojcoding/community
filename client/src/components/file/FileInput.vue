499467358247ad63004bf47770bfb250546cd96b
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-09-04 15:25:20 +0900

    파일 첨부 부분 수정
    기존에는 javascript를 이용하여 dom을 조작해 html요소들을 만들었으나 뷰의 데이터를 조작하여 html요소들이 생성되고 사자리도록 수정하였음.

--- client/src/components/file/FileInput.vue
+++ client/src/components/file/FileInput.vue
@@ -10,18 +10,46 @@
         가능합니다.
       </td>
     </tr>
-    <tr v-for="file in fileListData" :key="file.fileId">
+    <tr v-for="(file, index) in uploadedFiles" :key="file.fileId">
       <td class="align-middle img-cell">
         <img v-if="file.thumbnailName"
              :src="`${dynamicDomain}/api/v1/files/images/${file.thumbnailName}`"
-             alt="Image">
+        >
         <a :id="'downloadTag_' + file.fileId"
+           class="text-decoration-none"
            @click="downloadFile(file.fileId)">
           <span>{{ file.originalName }}</span>
         </a>
       </td>
       <td>
-        <button type="button" class="btn btn-sm btn-danger" @click="removeFile(file.fileId)">X</button>
+        <button type="button" class="btn btn-sm btn-danger"
+                @click="removeFile(index, file.fileId)">X
+        </button>
+      </td>
+    </tr>
+    <tr v-for="(file, index) in inputFiles" :key="'input_' + index">
+      <td class="align-middle img-cell">
+        <img v-if="type === 'gallery' && thumbnails[index]"
+             :src="thumbnails[index]" alt="Image">
+        <input
+            v-if="type === 'help'"
+            type="file"
+            @change="handleFileChange($event, index)"
+            class="form-control-file"
+            accept=".jpg, .jpeg, .gif, .png, application/zip"
+        >
+        <input
+            v-else
+            type="file"
+            @change="handleFileChange($event, index); showThumbnail($event)"
+            class="form-control-file"
+            accept=".jpg, .jpeg, .gif, .png"
+        >
+      </td>
+      <td>
+        <button type="button" class="btn btn-sm btn-danger"
+                @click="removeInputFile(index)">X
+        </button>
       </td>
     </tr>
     </tbody>
@@ -52,13 +80,18 @@ export default {
       description: '파일 리스트'
     },
   },
+  created() {
+    this.uploadedFiles = this.fileListData;
+  },
   emits: ['files-updated', 'files-deleted'],
   data() {
     return {
-      files: [],
+      inputFiles: [],
+      thumbnails: [],
+      uploadedFiles: [],
       deleteFileIds: [],
       MAX_FILES: this.type === 'gallery' ? 20 : 5,
-      fileCount: this.fileListData ? this.fileListData.length : 0
+      fileCount: this.uploadedFiles ? this.uploadedFiles.length : 0
     };
   },
   setup() {
@@ -74,106 +107,46 @@ export default {
      * 데이터 변동 사항을 부모 컴포넌트로 emit할 수 있도록 이벤트 리스너 추가
      */
     addFileInput() {
-      if (this.fileCount >= this.MAX_FILES) {
+      if (this.fileCount + this.inputFiles.length >= this.MAX_FILES) {
         alert('최대 ' + this.MAX_FILES + '개까지만 첨부 가능합니다.');
+
         return;
       }
 
-      const fileIndex = this.files.length;
-
-      const fileInputTable = document.getElementById('fileInputTable');
-
-      const newRow = document.createElement('tr');
-
-      const inputCell = document.createElement('td');
-      inputCell.classList.add('align-middle', 'image-cell', 'd-flex', 'justify-content-start');
-
-      const removeButtonCell = document.createElement('td');
-
-      const inputField = document.createElement('input');
-      inputField.setAttribute('type', 'file');
-      inputField.setAttribute('id', 'fileInput_' + fileIndex);
-      inputField.classList.add('form-control-file');
-      inputField.addEventListener('change', (event) => this.handleFileChange(event, fileIndex));
-
-      if (this.type === 'gallery') {
-        inputField.setAttribute('accept', '.jpg, .jpeg, .gif, .png');
-        inputField.addEventListener('change', () => this.showThumbnail(fileIndex));
-
-        const thumbnailImg = document.createElement('img');
-        thumbnailImg.setAttribute('id', 'thumbnailImg_' + fileIndex);
-        thumbnailImg.classList.add('img-cell');
-        thumbnailImg.style.width = '60px';
-        thumbnailImg.style.height = '60px';
-
-        inputCell.appendChild(thumbnailImg);
-      } else {
-        inputField.setAttribute('accept', '.jpg, .jpeg, .gif, .png, application/zip');
-      }
-
-      const removeButton = document.createElement('button');
-      removeButton.setAttribute('type', 'button');
-      removeButton.classList.add('btn', 'btn-danger', 'btn-sm');
-      removeButton.innerText = 'X';
-
-      removeButton.addEventListener('click', () => {
-        this.handleFileRemove(fileIndex);
-
-        fileInputTable.removeChild(newRow);
-
-        this.fileCount--;
-      });
-
-      inputCell.appendChild(inputField);
-
-      removeButtonCell.appendChild(removeButton);
-
-      newRow.appendChild(inputCell);
-      newRow.appendChild(removeButtonCell);
-
-      fileInputTable.appendChild(newRow);
-
-      this.fileCount++;
+      this.inputFiles.push({});
     },
-
     /**
      * 파일의 변동 사항을 부모 컴포넌트로  emit 하는 메서드
-     *
      * @param event
-     * @param fileIndex
+     * @param index
      */
-    handleFileChange(event, fileIndex) {
+    handleFileChange(event, index) {
       const selectedFile = event.target.files[0];
-      this.files[fileIndex] = selectedFile;
-      this.$emit('files-updated', this.files);
+
+      if (selectedFile) {
+        this.inputFiles[index] = selectedFile;
+
+        this.$emit('files-updated', this.inputFiles);
+      }
     },
     /**
      * 파일 변동 사항을 부모 컴포넌트로  emit 하는 메서드
      *
-     * @param fileIndex
+     * @param index
      */
-    handleFileRemove(fileIndex) {
-      this.files.splice(fileIndex, 1);
-      this.$emit('files-updated', this.files);
+    removeInputFile(index) {
+      // 입력란 제거
+      this.inputFiles.splice(index, 1);
+
+      this.$emit('files-updated', this.inputFiles);
     },
     /**
      * 저장된 파일을 삭제하기 위한 메서드
      *
      * @param fileId
      */
-    removeFile(fileId) {
-      // <a> 태그가 속한 tr제거
-      const downloadTag = document.getElementById('downloadTag_' + fileId);
-
-      if (downloadTag) {
-        const tr = downloadTag.closest('tr');
-
-        if (tr) {
-          tr.remove();
-        }
-      }
-
-      this.fileCount--;
+    removeFile(index, fileId) {
+      this.uploadedFiles.splice(index, 1);
 
       this.deleteFileIds.push(fileId);
 
@@ -182,27 +155,20 @@ export default {
     /**
      * 썸네일을 보여주기 위한 메서드
      *
-     * @param fileIndex
+     * @param event
      */
-    showThumbnail(fileIndex) {
-      const fileInput = document.getElementById('fileInput_' + fileIndex);
-
-      const thumbnailImg = document.getElementById('thumbnailImg_' + fileIndex);
-
-      thumbnailImg.src = '';
-
-      const file = fileInput.files[0];
+    showThumbnail(event) {
+      const file = event.target.files[0];
 
       const imageTypes = ['image/jpeg', 'image/gif', 'image/png'];
 
       if (file && imageTypes.includes(file.type)) {
         const reader = new FileReader();
-
-        reader.onload = function (e) {
-          thumbnailImg.src = e.target.result;
-        };
-
         reader.readAsDataURL(file);
+
+        reader.onload = () => {
+          this.thumbnails.push(reader.result);
+        }
       }
     },
     /**
@@ -218,12 +184,16 @@ export default {
         alert(error);
       }
     },
-  },
-};
+  }
+}
 </script>
 
 <style scoped>
 .img-cell {
   text-align: left;
+}
+img {
+  height: 60px;
+  width: 60px;
 }
 </style>
